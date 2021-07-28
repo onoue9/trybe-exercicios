8e0907f4401665fa1eced9915b758ccce36640e5
onoue <filipeflom@gmail.com>
2021-07-28 19:03:45 -0300

    exercicio 10 realizado

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa13.js
@@ -0,0 +1,16 @@
+//exercício 10
+let custoDoProduto = 100;
+let valorDeVenda = 200;
+let valorCustoTotal;
+let lucro;
+let lucroTotal;
+
+valorCustoTotal = custoDoProduto + (custoDoProduto*0.2);
+
+lucro = valorDeVenda - valorCustoTotal;
+
+lucroTotal = lucro * 1000;
+
+console.log(valorCustoTotal);
+console.log(lucro);
+console.log(lucroTotal);
