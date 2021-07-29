ff64662b4bd6e5390ffc8c37127f591a0d1608a7
onoue <filipeflom@gmail.com>
2021-07-29 16:59:22 -0300

    exercicio de fixacao 3 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -1,4 +1,4 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf('Portfólio');
+menu.push("Contato");
 
-console.log(indexOfPortfolio);
+console.log(menu);
