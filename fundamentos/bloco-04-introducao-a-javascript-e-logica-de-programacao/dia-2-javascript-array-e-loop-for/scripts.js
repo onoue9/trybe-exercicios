83742b509289065d2c77ad7d09179ac1468eea87
onoue <filipeflom@gmail.com>
2021-07-29 18:19:25 -0300

    exercicio 3 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,9 +1,10 @@
-//exercício 2
+//exercício 3
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
 for(let index = 0; index < numbers.length; index += 1){
-  soma += numbers[index];
+  soma += numbers[index];  
 }
 
+soma /= numbers.length
 console.log(soma);
