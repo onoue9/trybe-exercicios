1c4335e636b3f42acdc912bf94752372ddea7e87
onoue <filipeflom@gmail.com>
2021-07-29 19:04:46 -0300

    exercicio bonus 2 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,10 +1,10 @@
-//exercício bônus 1
+//exercício bônus 2
 let numbers = 
 [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 
 for (let index = 1; index < numbers.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (numbers[index] < numbers[secondIndex]) {
+    if (numbers[index] > numbers[secondIndex]) {
       let position = numbers[index];
       numbers[index] = numbers[secondIndex];
       numbers[secondIndex] = position;
