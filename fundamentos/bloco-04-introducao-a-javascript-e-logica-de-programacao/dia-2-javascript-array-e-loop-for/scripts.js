a378501dfdc20c8638dbd8f0d0458aef67158462
onoue <filipeflom@gmail.com>
2021-07-29 18:52:52 -0300

    exercicio 9 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,8 +1,15 @@
-//exercício 8
+//exercício 9
 let numeros = [];
+let divisao = [];
 
 for(let index = 1; index < 26; index += 1){
   numeros.push(index);
 }
 
+for(let indexDiv = 0; indexDiv < numeros.length; indexDiv += 1){
+  divisao.push(numeros[indexDiv] / 2);
+}
+
 console.log(numeros);
+console.log();
+console.log(divisao);
