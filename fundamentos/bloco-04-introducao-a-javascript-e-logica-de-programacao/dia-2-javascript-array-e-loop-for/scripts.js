a5c57454680d425deac560a6dcc63831152fb9a5
onoue <filipeflom@gmail.com>
2021-07-29 18:49:10 -0300

    exercicio 8 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,19 +1,8 @@
-//exercício 7
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorValor=3;
+//exercício 8
+let numeros = [];
 
-for(let index = 0; index < numbers.length; index += 1){
-  let valor = numbers[index];
-  for(let indexMenor = 0; indexMenor < numbers.length; indexMenor += 1){
-    if(valor < numbers[indexMenor]){
-      let valorMenor = valor;
+for(let index = 1; index < 26; index += 1){
+  numeros.push(index);
+}
 
-    if(valorMenor < menorValor){
-      menorValor = valorMenor;
-      }
-    }
-  }
-}  
-
-
-console.log(menorValor);
+console.log(numeros);
