7c2735f5a01d8a9df90e289777883278e05534dc
onoue <filipeflom@gmail.com>
2021-07-29 18:32:14 -0300

    exercicio 5 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,16 +1,18 @@
-//exercício 3
+//exercício 5
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let maiorValor = 0;
 
 for(let index = 0; index < numbers.length; index += 1){
-  soma += numbers[index];  
+  let valor = numbers[index];
+  for(let indexValor = 0; indexValor < numbers.length; indexValor += 1){
+    if(valor > numbers[indexValor]){
+      let valorMaior = valor;
+    
+    if(valorMaior > maiorValor){
+      maiorValor = valorMaior;
+    }
+  }
+  }
 }
 
-soma /= numbers.length
-
-if(soma > 20){
-  console.log("valor maior que 20");
-}
-else {
-  console.log("valor menor ou igual a 20");
-}
+console.log(maiorValor);
