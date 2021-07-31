efa177bb0b5895d1e6c3c573aaf872b6a11eebd6
onoue <filipeflom@gmail.com>
2021-07-31 17:39:14 -0300

    exercicio 2 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,11 +1,14 @@
-//exercício 1
+//exercício 2
 let size = 5;
 let quadrado = "";
 
 for(let indexLinha = 0; indexLinha < size; indexLinha += 1){  
-  for(let indexColuna = 0; indexColuna < size; indexColuna += 1){
-    quadrado += "*";
-  }
+  quadrado += "*";
+  /* for(let indexColuna = 0; indexColuna < size; indexColuna += 1){
+    quadrado += "*"
+    } */
+  /* quadrado = ""; */
   console.log(quadrado);
-  quadrado = "";
 }
+
+
