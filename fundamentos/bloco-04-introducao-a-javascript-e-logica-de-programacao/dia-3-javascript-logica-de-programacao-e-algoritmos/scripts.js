78bda9852b0f643abfc12c852ab5c99431d1ad8c
onoue <filipeflom@gmail.com>
2021-08-01 18:09:00 -0300

    exercicio 5 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,18 +1,25 @@
-//exercício 3
-let n = 5;
+//exercício 5
+let n = 21;
 let symbol = "*";
 let inputLine = "";
-let inputPosition = n;
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+let controlMidRight = midOfMatrix;
+let controlMidLeft = midOfMatrix;
 
-for(let lineIndex = 0; lineIndex < n; lineIndex += 1){
+for(let lineIndex = 0; lineIndex < midOfMatrix; lineIndex += 1){
   for(let columnIndex = 0; columnIndex <= n; columnIndex += 1){
-    if(columnIndex < inputPosition){
-      inputLine = inputLine + " ";
-    }else {
+    if(columnIndex == controlLeft || columnIndex == controlRight || lineIndex == midOfMatrix - 1 && columnIndex > 0){
       inputLine = inputLine + symbol;
+    }else {
+      inputLine = inputLine + " ";
     }
   }
   console.log(inputLine);
   inputLine = "";
-  inputPosition -= 1;
+  controlRight += 1;
+  controlLeft -= 1;
+  controlMidRight += 1;
+  controlMidLeft -= 1;
 }
