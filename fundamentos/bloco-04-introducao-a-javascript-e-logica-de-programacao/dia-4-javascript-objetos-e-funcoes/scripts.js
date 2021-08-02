3a7eb0342526824faab4356687ba2ae7372027ba
onoue <filipeflom@gmail.com>
2021-08-02 15:09:51 -0300

    exercicio fixacao 3 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -6,3 +6,5 @@ let player = {
 }
 
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
+
+player['bestInTheWorld'] = { years: [2006, 2007, 2008, 2009, 2010, 2018]};
