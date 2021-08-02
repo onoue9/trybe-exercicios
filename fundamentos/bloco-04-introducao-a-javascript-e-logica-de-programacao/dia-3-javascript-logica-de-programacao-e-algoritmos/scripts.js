c873b72bc58358f1633ef953079d6d120c3fc6ee
onoue <filipeflom@gmail.com>
2021-08-02 09:50:40 -0300

    exercicio 6 realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,25 +1,14 @@
-//exercício 5
-let n = 21;
-let symbol = "*";
-let inputLine = "";
-let midOfMatrix = (n + 1) / 2;
-let controlLeft = midOfMatrix;
-let controlRight = midOfMatrix;
-let controlMidRight = midOfMatrix;
-let controlMidLeft = midOfMatrix;
+//exercício 6
+let number = 23;
+let primo = [];
 
-for(let lineIndex = 0; lineIndex < midOfMatrix; lineIndex += 1){
-  for(let columnIndex = 0; columnIndex <= n; columnIndex += 1){
-    if(columnIndex == controlLeft || columnIndex == controlRight || lineIndex == midOfMatrix - 1 && columnIndex > 0){
-      inputLine = inputLine + symbol;
-    }else {
-      inputLine = inputLine + " ";
-    }
+for(let index = 0; index <= number; index += 1){
+  if(number%index == 0){
+    primo.push(number);
   }
-  console.log(inputLine);
-  inputLine = "";
-  controlRight += 1;
-  controlLeft -= 1;
-  controlMidRight += 1;
-  controlMidLeft -= 1;
+}
+if(primo.length <= 2){
+  console.log("É primo.")
+}else{
+  console.log("Não é primo.")
 }
