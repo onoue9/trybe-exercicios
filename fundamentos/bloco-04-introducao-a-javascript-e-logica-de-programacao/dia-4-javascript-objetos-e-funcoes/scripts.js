978ee9105d517813a5e6c1686edda687d8a0c1b2
onoue <filipeflom@gmail.com>
2021-08-02 15:22:22 -0300

    exercicio fixacao 1 for/in realizado

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -1,14 +1,9 @@
-let player = {
-  name: 'Marta',
-  lastName: 'Silva',
-  age: 34,
-  medals: { golden: 2, silver: 3 }
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for(let key in names){
+  console.log("Olá " + names[key]);
 }
-
-console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
-
-player['bestInTheWorld'] = { years: [2006, 2007, 2008, 2009, 2010, 2018]};
-
-console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.years.length + " vezes.");
-
-console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " de prata.");
